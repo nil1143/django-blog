52a72648d73ba86ceafcc93b2f1b7fba2f49f422
Tom Nil <tnil1143@gmail.com>
2024-05-24 03:11:48 +0000

    debugging

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["8000-nil1143-djangoblog-gssvf315wqb.ws-eu111.gitpod.io", '.herokuapp.com']
+ALLOWED_HOSTS = ["8000-nil1143-djangoblog-gssvf315wqb.ws-eu114.gitpod.io", '.herokuapp.com']
 
 
 # Application definition
