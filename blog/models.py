b1a77e65e49dfc6b81db4b4911a2341ac40d14bc
Tom Nil <tnil1143@gmail.com>
2024-05-31 01:08:47 +0000

    comments displaying under the posts

--- blog/models.py
+++ blog/models.py
@@ -26,7 +26,7 @@ class Post(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="blog_comments"
+        Post, on_delete=models.CASCADE, related_name="comments"
     )
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
